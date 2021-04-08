359da5916cb9869e00985fb09c1bdc2c8d8029f8
johnjeang <johnjeang@gmail.com>
2021-04-07 19:25:24 -0500

    Initial Commit

---
+++ bullseye/bullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyeApp.swift
+//  bullseye
+//
+//  Created by John Jeang on 4/7/21.
+//
+
+import SwiftUI
+
+@main
+struct bullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
